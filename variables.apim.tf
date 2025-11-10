6d0544095d9bb487237a9beaa90b9997da30ad82
Marko Ikäheimo <marko.ikaheimo@epicalgroup.com>
2025-11-10 09:02:40 +0000

    Managed identity variables.

--- variables.apim.tf
+++ variables.apim.tf
@@ -5,6 +5,10 @@ variable "apim_instances" {
     publisher_email = string
     sku             = string
     resource_group  = string
+    managed_identities = optional(object({
+      system_assigned            = optional(bool, true) # Default enable System Assigned Identity
+      user_assigned_resource_ids = optional(set(string), [])
+    }), { system_assigned = false })
     diagnostic_settings = optional(map(object({
       workspace                      = string
       resource_group                 = string
