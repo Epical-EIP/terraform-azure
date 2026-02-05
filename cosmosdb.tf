48d3981e7656b46ccc359104dc53a3c8df1e0622
Marko Ikäheimo <marko.ikaheimo@epicalgroup.com>
2026-02-05 07:34:51 +0200

    Fixes to cosmosdb

--- cosmosdb.tf
+++ cosmosdb.tf
@@ -1,23 +1,28 @@
-module "vm-res-cosmosdb" {
-  source  = "Azure/avm-res-cosmosdb/azurerm"
-  version = "0.19.1"
+module "avm-res-documentdb-databaseaccount" {
+  source  = "Azure/avm-res-documentdb-databaseaccount/azurerm"
+  version = "0.6.0"
 
   for_each = contains(var.enabled_features, "cosmosdb") ? var.cosmosdb_accounts : {}
 
-  name                = join("-", ["cosmosdb", local.name_prefix, each.key, local.name_suffix])
-  location            = each.value.location != null ? each.value.location : var.location
-  resource_group_name = azurerm_resource_group.rg["${each.value.resource_group}"].name
-  account_kind        = each.value.account_kind
-  account_tier        = each.value.account_tier
-  account_replication_type = each.value.account_replication_type
-  
+  name                                  = join("-", ["cosmos", local.name_prefix, each.key, local.name_suffix])
+  location                              = each.value.location != null ? each.value.location : var.location
+  resource_group_name                   = azurerm_resource_group.rg["${each.value.resource_group}"].name
+  free_tier_enabled                     = each.value.free_tier_enabled
+  consistency_policy                    = each.value.consistency_policy
+  geo_locations                         = each.value.geo_locations
+  capabilities                          = each.value.capabilities
+  cors_rule                             = each.value.cors_rule
+  network_acl_bypass_for_azure_services = each.value.network_acl_bypass_for_azure_services
+  public_network_access_enabled         = each.value.public_network_access_enabled
+  managed_identities                    = each.value.managed_identities
+
   virtual_network_rules = each.value.virtual_network != null ? [
     {
       subnet_id = each.value.virtual_network != null && each.value.subnet != null ? module.avm-res-network-virtualnetwork-subnet["${each.value.virtual_network}-${each.value.subnet}"].resource_id : null
     }
-  ] : [] 
+  ] : []
 
-  ip_range_filter     = each.value.ip_range_filter
-  enable_telemetry    = var.enable_telemetry
-  tags                = var.default_tags
+  ip_range_filter  = each.value.ip_range_filter
+  enable_telemetry = var.enable_telemetry
+  tags             = merge(var.default_tags, each.value.tags)
 }
--- variables.cosmosdb.tf
+++ variables.cosmosdb.tf
@@ -1,30 +1,43 @@
 variable "cosmosdb_accounts" {
   type = map(object({
-    resource_group       = string
-    location             = optional(string, null)
-    offer_type           = optional(string, "Standard")
-    kind                 = optional(string, "GlobalDocumentDB")
-    free_tier_enabled   = optional(bool, false)
-    consistency_policy   = optional(object({
-      consistency_level       = string
-      max_interval_in_seconds = optional(number, null)
-      max_staleness_prefix    = optional(number, null)
+    resource_group    = string
+    location          = optional(string, null)
+    offer_type        = optional(string, "Standard")
+    kind              = optional(string, "GlobalDocumentDB")
+    free_tier_enabled = optional(bool, false)
+    consistency_policy = optional(object({
+      max_interval_in_seconds = optional(number, 5)
+      max_staleness_prefix    = optional(number, 100)
+      consistency_level       = optional(string, "ConsistentPrefix")
     }), { consistency_level = "Session" })
-    geo_locations        = optional(list(object({
+    geo_locations = optional(set(object({
       location          = string
       failover_priority = number
       is_zone_redundant = optional(bool, false)
     })), [])
-    enable_automatic_failover = optional(bool, false)
-    virtual_network = optional(string, null)
-    subnet          = optional(string, null)
-    ip_range_filter         = optional(string, null)
-    enable_multiple_write_locations = optional(bool, false)
-    capabilities            = optional(list(string), []) 
-    backup_policy_type      = optional(string, "Periodic") # Options: Periodic, Continuous
-    periodic_backup_interval_in_minutes = optional(number, 240) # Default 4 hours
-    periodic_backup_retention_in_hours   = optional(number, 8)   # Default 8 hours
-    continuous_backup_retention_in_days   = optional(number, 7)   # Default 7 days
-    tags                    = optional(map(string), {})
+    enable_automatic_failover           = optional(bool, false)
+    virtual_network                     = optional(string, null)
+    subnet                              = optional(string, null)
+    ip_range_filter                     = optional(string, null)
+    enable_multiple_write_locations     = optional(bool, false)
+    capabilities                        = optional(set(object({ name = string })), [])
+    backup_policy_type                  = optional(string, "Periodic") # Options: Periodic, Continuous
+    periodic_backup_interval_in_minutes = optional(number, 240)        # Default 4 hours
+    periodic_backup_retention_in_hours  = optional(number, 8)          # Default 8 hours
+    continuous_backup_retention_in_days = optional(number, 7)          # Default 7 days
+    tags                                = optional(map(string), {})
+    managed_identities = optional(object({
+      system_assigned            = optional(bool, true) # Default enable System Assigned Identity
+      user_assigned_resource_ids = optional(set(string), [])
+    }), { system_assigned = true })
+    cors_rule = optional(object({
+      allowed_headers    = set(string)
+      allowed_methods    = set(string)
+      allowed_origins    = set(string)
+      exposed_headers    = set(string)
+      max_age_in_seconds = optional(number, null)
+    }), null)
+    network_acl_bypass_for_azure_services = optional(bool, false)
+    public_network_access_enabled         = optional(bool, false)
   }))
 }
