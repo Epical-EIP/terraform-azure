ca7a9daa4fd2ab5417ba1a365faa47796fc175a7
Marko Ikäheimo <marko.ikaheimo@epicalgroup.com>
2026-08-11 08:55:15 +0300

    Update flex function app default runtime .NET to 10.0

--- variables.functionapp.tf
+++ variables.functionapp.tf
@@ -7,7 +7,7 @@ variable "function_apps" {
     location                      = optional(string, null)
     enable_telemetry              = optional(bool, false)
     fc1_runtime_name              = optional(string, "dotnet-isolated") # Default .NET Isolated
-    fc1_runtime_version           = optional(string, "8.0")             # Default .NET 8
+    fc1_runtime_version           = optional(string, "10.0")            # Default .NET 10
     function_app_uses_fc1         = optional(bool, true)                # Default uses Flex Consumption Plan
     functions_extension_version   = optional(string, "~4")              # Default Azure Functions runtime Version 4
     https_only                    = optional(bool, true)                # Default HTTPS only
